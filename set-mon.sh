b09dfd3d9902659675116c5f6d5022d3795ca28b
Lu-Chi <fra.proxy@gmail.com>
2016-02-29 23:18:50 +0100

    Added default values.

--- set-mon.sh
+++ set-mon.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 # set -x 
 
-[ -z $1 ] && { echo "No param: wlan interface..."; exit 1; }
+[ -z ${1} ] && { echo "No param: wlan interface..."; exit 1; }
 
-iface=$1
-count=3
+iface=${1}
+count=${2:=3}
 
 for ((i=1; i<=$count; i++))
 do
-    iw $iface interface add mon$i type monitor
-    macchanger -r mon$i
+    iw ${iface} interface add mon${i} type monitor
+    macchanger -r mon${i}
 done
 
 
